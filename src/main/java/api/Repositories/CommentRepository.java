ab6765126e0ec8f785a8ac82b48378d5534bc420
Hüseyin Türkmen <huseyinturkmen2882@gmail.com>
2022-09-18 15:37:22 +0300

    update

--- src/main/java/api/Repositories/CommentRepository.java
+++ src/main/java/api/Repositories/CommentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-    //repository neden interface olmak zorunda
+
 
     List<Comment> findByUserIdAndPostId(Long userId,Long postId);
     List<Comment> findByUserId(Long userId);
